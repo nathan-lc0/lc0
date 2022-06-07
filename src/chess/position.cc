dfcd88b88ffb93146d6ff5e7558a447e23002dfa
nathan-lc0 <98367568+nathan-lc0@users.noreply.github.com>
2022-06-07 15:34:14 -0500

    Update position.cc

--- src/chess/position.cc
+++ src/chess/position.cc
@@ -102,7 +102,6 @@ GameResult PositionHistory::ComputeGameResult() const {
   }
 
   if (!board.HasMatingMaterial()) return GameResult::DRAW;
-  if (Last().GetRule50Ply() >= 100) return GameResult::DRAW;
   if (Last().GetRepetitions() >= 2) return GameResult::DRAW;
 
   return GameResult::UNDECIDED;
